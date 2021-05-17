46d3534f99a20d5e3dfb9167c4791e89dd8fd20d
klaudiafaltyn <112101@student.upwr.edu.pl>
2021-05-17 22:48:27 +0200

    created main component

--- stats-preview-card-component/src/InfoCard.js
+++ stats-preview-card-component/src/InfoCard.js
@@ -1,52 +1,43 @@
 import React, { Suspense } from "react";
 import "./InfoCard.scss";
-// import { useImage } from "react-image";
 import Photo from "./image-header-desktop.jpg";
 
-// const MyImageComponent = () => {
-//   const { src } = useImage({
-//     srcList: "image-header-desktop.jpg",
-//   });
-
-//   return <img src={src} />;
-// };
+const Tile = ({ textTop, textBottom }) => {
+  return (
+    <span className="tile">
+      <p className="tile__top">{textTop}</p>
+      <p className="tile__bottom">{textBottom}</p>
+    </span>
+  );
+};
 
 const InfoCard = () => {
-  const violetWord = (props) => (
-    <p style={{ fontWeight: "bold", fontColor: "$soft-violet" }}>
-      {props.children}
-    </p>
-  );
-
   return (
     <div className="bcg">
       <div className="container">
         <div className="txtFields">
-          <div className="hugeTxtField">
-            <p>
-              Get <violetWord>insights</violetWord> that help <br /> your
-              business grow.{" "}
-            </p>
-          </div>
+          <div className="subContainer">
+            <div className="hugeTxtField">
+              Get <span className="violetWord">insights</span> that help your
+              business grow.
+            </div>
 
-          <div className="smallTxtField">
-            {" "}
-            Discover the benefits of data analytics and make <br /> better
-            decisions regarding revenue, customer <br /> experience, and overall
-            efficiency.{" "}
-          </div>
-          <div className="numbers">
-            10k+ companies 314 templates 12m+ queries
+            <div className="smallTxtField">
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </div>
+            <div className="numbers">
+              <Tile textTop="10k+" textBottom="companies" />
+              <Tile textTop="314" textBottom="templates" />
+              <Tile textTop="12M+" textBottom="queries" />
+            </div>
           </div>
         </div>
         <div className="violetImg">
-          <img className='photo' src={Photo} alt="Logo" />
+          <div className="violetOpacity"></div>
+          <img className="photo" src={Photo} alt="photo" />
         </div>
       </div>
-
-      {/* <Suspense fallback={<h1>image</h1>}>
-      <MyImageComponent />
-    </Suspense> */}
     </div>
   );
 };
